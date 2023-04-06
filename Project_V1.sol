f9aa2db2539d3f1e70a29b27135ebc31bdc9f6c9
AngelHL02 <hei_laam@yahoo.com.hk>
2023-04-06 10:28:48 +0800

    6 Apr 10:30

--- Project_V1.sol
+++ Project_V1.sol
@@ -237,14 +237,15 @@ contract medical{
     }
 
     //verifies the signature
-    using ECDSA for bytes32; 
+    using ECDSA for bytes32;
 
     // Recover the signer's address from the hash and signature
-    function verifySignature(bytes32 hash, bytes memory signature, address expectedSigner) public pure returns (bool) { 
+    function verifySignature(bytes32 hash, bytes memory signature, address expectedSigner) 
+        public pure returns (bool,address) { 
         // Compare the recovered address with the expected signer 
         address signer = hash.recover(signature); 
         
-        return signer == expectedSigner; 
+        return (signer == expectedSigner,signer); 
 
     } 
 
